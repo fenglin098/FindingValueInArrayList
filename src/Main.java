44f772a50c347f513d332a9edc4e63174cd69cc8
feng.lin098 <feng.lin098@gmail.com>
2019-10-28 21:02:07 -0400

    third commit

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,25 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        int count=0;
         List<Integer> intArray = Arrays.asList(24, 30, 31, 24, 32, 33, 34, 24, 35, 36);
         ArrayList<Integer> arr1 = new ArrayList<>();
         arr1.addAll(intArray);
         System.out.println("Search for a number: ");
         Scanner sc = new Scanner(System.in);
         int userInput = sc.nextInt();
-        for (int item : arr1) {
-            int searchIndex = Collections.binarySearch(arr1, userInput);
+        int searchIndex = Collections.binarySearch(arr1, userInput);
             if (searchIndex>=0) {
-                int count=0;
-                count+=Collections.frequency();
-                System.out.println(userInput + " was found " + count + " times.");
+                for (int i=0; i<intArray.size();i++) {
+                    if (intArray.get(i) == userInput) {
+                        count++;
+                    } else {
+                        break;
+                    }
+                    System.out.println(userInput + " was found " + count + " times.");
+                }
             } else {
                 System.out.println(userInput + " was not found.");
             }
         }
-    }
 }
